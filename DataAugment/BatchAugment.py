10299d0295dd4afa19e9b260a4daa8923e59f640
Xv-M-S <1205507925@qq.com>
2024-08-29 17:08:32 +0800

    修改图片名和文件名不对应的bug

--- DataAugment/BatchAugment.py
+++ DataAugment/BatchAugment.py
@@ -35,7 +35,7 @@ def process_dataset(data_dir, output_dir, times=1):
             img_path = os.path.join(image_split_dir, img_file)
             label_path = os.path.join(label_split_dir, os.path.splitext(img_file)[0] + '.txt')
             output_img_path = os.path.join(output_image_split_dir, img_file)
-            output_label_path = os.path.join(output_label_split_dir, os.path.splitext(img_file)[0] + '.txt')
+            # output_label_path = os.path.join(output_label_split_dir, os.path.splitext(img_file)[0] + '.txt')
 
             # 读取标注
             with open(label_path, 'r') as f:
@@ -49,6 +49,7 @@ def process_dataset(data_dir, output_dir, times=1):
                     # 裁剪区域
                     wh, ht = image.size
                     class_id, x, y, w, h = [float(x) for x in label]
+                    class_id = int(class_id)
                     x, y, w, h = int(x * wh), int(y * ht), int(w * wh), int(h * ht)
                     bbox = (x - w // 2, y - h // 2, w, h)
                     # 裁剪图像
@@ -69,7 +70,12 @@ def process_dataset(data_dir, output_dir, times=1):
                     img = Image.open("aug_output_image.jpg")
                     width, height = img.size
 
-                    aug_img_path = os.path.join(output_image_split_dir, f"{Path(img_file).stem}_{random.randint(0, 99999)}.jpg")
+                    aug_img_name = f"{Path(img_file).stem}_{random.randint(0, 99999)}.jpg"
+                    aug_img_path = os.path.join(output_image_split_dir, aug_img_name)
+                    output_label_name = f"{Path(aug_img_name).stem}.txt"
+                    output_label_path = os.path.join(output_label_split_dir, output_label_name)
+                    # print(aug_img_path)
+                    # print(output_label_path)
                     black_rectangle = Image.new("RGB", (right - left, bottom - top), (0, 0, 0))
                     image.paste(black_rectangle, (left, top))
                     image.paste(img, (x-width//2, y-height//2))
@@ -81,7 +87,7 @@ def process_dataset(data_dir, output_dir, times=1):
                         f.write(line)
 
 if __name__ == '__main__':
-    data_dir = '/data01/migu/soccer-track/Yolov8_Formatted_DataSets/百度飞桨/strong_dataset'
-    output_dir = '/data01/migu/soccer-track/Yolov8_Formatted_DataSets_Augmented/百度飞桨/strong_dataset'
+    data_dir = '/data01/migu/soccer-track/Yolov8_Formatted_DataSets/百度飞桨/final_dataset'
+    output_dir = '/data01/migu/soccer-track/Yolov8_Formatted_DataSets_Augmented/百度飞桨/final_dataset'
     process_dataset(data_dir, output_dir, times=1)
     os.system("rm output_image.jpg aug_output_image.jpg")
